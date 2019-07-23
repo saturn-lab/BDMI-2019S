06a7a10a1ce6230b467be532a17c1d0c89c34db8
Lucus7 <46985491+Lucus7@users.noreply.github.com>
2019-07-23 09:48:38 +0800

    Add files via upload

---
+++ fibonacci-蒋广宇.hs
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg936\cocoartf1671\cocoasubrtf500
+{\fonttbl\f0\fswiss\fcharset0 ArialMT;}
+{\colortbl;\red255\green255\blue255;\red62\green62\blue62;\red244\green246\blue249;}
+{\*\expandedcolortbl;;\cssrgb\c30980\c30980\c30980;\cssrgb\c96471\c97255\c98039;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh17500\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+let fibonacci n= if n<3 then 1 else fibonacci(n-1)+fibonacci(n-2)\
+Prelude> fibonacci 30}

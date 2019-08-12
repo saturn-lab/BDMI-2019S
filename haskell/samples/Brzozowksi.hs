58c89695b8c9ed70f1fcc572fc71f998c3de7df9
dregjust <42797488+dregjust@users.noreply.github.com>
2019-08-12 16:12:11 +0800

    Add files via upload

--- haskell/samples/Brzozowksi.hs
+++ haskell/samples/Brzozowksi.hs
@@ -12,7 +12,7 @@ out_1 (CharRE c) = [c]
 out_1 (SeqRE re_one re_two) =  (out_1 re_one)++(out_1 re_two)
 out_1 (ChoiceRE re_one re_two) = "("++(out_1 re_one)++"|"++(out_1 re_two)++")"
 out_1 (StarRE r) = 
-  if length (out r) == 1 then (out_1 r)++"*" else "("++(out_1 r)++")*"
+  if length (out_1 r) == 1 then (out_1 r)++"*" else "("++(out_1 r)++")*"
 out_1 VoidRE = "VoidRE" 
 out_1 EmptyRE = "EmptyRE"
 -- END:out_1
@@ -21,50 +21,78 @@ out_1 EmptyRE = "EmptyRE"
 out_2 :: Regexp -> String
 out_2 n =
  if (delta n)==True
- then (out_1 n)++"(End)"
+ then (out_1 n)++"(END)"
  else out_1 n
 --END:out_2
 
--- START:out
-out :: Regexp -> String
-out n = 
- -- let n' = out_2 n--
+-- START:out_3
+out_3 :: Regexp -> String
+out_3 n = 
+ let n' = out_2 n
+ in case n' of
+   "VoidRE" -> "Err"
+   _ -> n'
+-- END:out_3
+
+-- START:out_4
+out_4 :: Regexp -> String
+out_4 n = 
  let n' = out_1 n
  in case n' of
    "VoidRE" -> "Err"
    _ -> n'
--- END:out
+-- END:out_4
+
+--START:if_empty
+if_empty :: Regexp -> Bool
+if_empty EmptyRE = True
+if_empty VoidRE = False
+if_empty n = 
+ if (derivative n 'a')==n
+    then if (derivative n 'b')==n
+           then False
+           else (if_empty (derivative n 'b'))
+    else if (derivative n 'b')==n
+           then (if_empty (derivative n 'a'))
+           else (if_empty (derivative n 'a'))||(if_empty (derivative n 'b'))
+--END:if_empty
 
 -- START:fsmcal
 fsmcal :: Regexp -> [Fsm]
-fsmcal (CharRE c) = 
- [Fsmsin (CharRE c) 'a' (derivative (CharRE c) 'a'), Fsmsin (CharRE c) 'b' (derivative (CharRE c) 'b')]
 fsmcal VoidRE = [] 
 fsmcal EmptyRE = []
 fsmcal n =
- if (derivative n 'a')==n
+  if (derivative n 'a')==n
     then if (derivative n 'b')==n
-        then [Fsmsin n 'a' (derivative n 'a')]++[Fsmsin n 'b' (derivative n 'b')]
-        else [Fsmsin n 'a' (derivative n 'a')]++[Fsmsin n 'b' (derivative n 'b')]++(fsmcal (derivative n 'b'))
+           then [Fsmsin n 'a' (derivative n 'a')]++[Fsmsin n 'b' (derivative n 'b')]
+           else [Fsmsin n 'a' (derivative n 'a')]++[Fsmsin n 'b' (derivative n 'b')]++(fsmcal (derivative n 'b'))
     else if (derivative n 'b')==n
-        then [Fsmsin n 'a' (derivative n 'a')]++[Fsmsin n 'b' (derivative n 'b')]++(fsmcal (derivative n 'a'))
-        else (fsmcal (derivative n 'a'))++(fsmcal (derivative n 'b'))++[Fsmsin n 'a' (derivative n 'a')]++[Fsmsin n 'b' (derivative n 'b')]
+           then [Fsmsin n 'a' (derivative n 'a')]++[Fsmsin n 'b' (derivative n 'b')]++(fsmcal (derivative n 'a'))
+           else (fsmcal (derivative n 'a'))++(fsmcal (derivative n 'b'))++[Fsmsin n 'a' (derivative n 'a')]++[Fsmsin n 'b' (derivative n 'b')]
 -- END:fsmcal
+
+--START:fsmcal_re
+fsmcal_re :: Regexp -> [Fsm]
+fsmcal_re n = 
+ if (if_empty n)==True
+  then (fsmcal n)++[Fsmsin VoidRE 'a' VoidRE,Fsmsin VoidRE 'b' VoidRE]++[Fsmsin EmptyRE 'a' VoidRE,Fsmsin EmptyRE 'b' VoidRE]
+  else (fsmcal n)++[Fsmsin VoidRE 'a' VoidRE,Fsmsin VoidRE 'b' VoidRE]
+--END:fsmcal_re
 
 --格式化输出
 valueOf :: Fsm -> String
-valueOf (Fsmsin a b c)=(out a)++"--"++[b]++"--"++(out c)
+valueOf (Fsmsin a b c)=(out_4 a)++"--"++[b]++"--"++(out_3 c)
 
 listValues :: [Fsm] -> [String]
 listValues = map valueOf
 
 printFsm :: Regexp -> IO ()
 printFsm = 
- putStrLn . unlines . listValues . fsmcal
+ putStrLn . unlines . listValues . fsmcal_re
 --格式化输出
 
 
-
+--Fsmsin EmptyRE 'a' VoidRE,Fsmsin EmptyRE 'b' VoidRE
 
 
 
---
+++ haskell/samples/Deriv.hs
@@ -0,0 +1,60 @@
+module Deriv where
+
+-- START:data
+data Regexp = CharRE Char
+            | ChoiceRE Regexp Regexp
+            | SeqRE Regexp Regexp
+            | StarRE Regexp
+            | VoidRE
+            | EmptyRE
+            deriving (Eq, Show)
+-- END:data
+
+-- START:delta
+delta :: Regexp -> Bool
+delta (CharRE c) = False -- (1)
+delta (ChoiceRE re_one re_two) = -- (2)
+  (delta re_one) || (delta re_two)
+delta (SeqRE re_one re_two) =  -- (3)
+  (delta re_one) && (delta re_two)
+delta (StarRE r) = True -- (4)
+delta VoidRE = False -- (5)
+delta EmptyRE = True -- (6)
+-- END:delta
+
+-- START:deriv
+derivative :: Regexp -> Char -> Regexp
+derivative (CharRE c) d = -- (7)
+  if c == d
+    then EmptyRE
+    else VoidRE
+derivative (SeqRE re_one re_two) c = -- (8)
+  let re_one' = (derivative re_one c)
+      re_two' = (derivative re_two c)
+  in case re_one' of
+    VoidRE -> if (delta re_one)
+                then (derivative re_two c)
+                else VoidRE
+    EmptyRE -> re_two
+    _ -> if (delta re_one)
+           then if (re_two'==VoidRE)
+                  then SeqRE re_one' re_two
+                  else (ChoiceRE (SeqRE re_one' re_two) (derivative re_two c))
+           else (SeqRE re_one' re_two)
+derivative (ChoiceRE re_one re_two) c = -- (9)
+  let re_one' = (derivative re_one c)
+      re_two' = (derivative re_two c)
+  in case (re_one', re_two') of
+    (VoidRE, VoidRE) -> VoidRE
+    (VoidRE, nonvoid) -> nonvoid
+    (nonvoid, VoidRE) -> nonvoid
+    _ -> (ChoiceRE re_one' re_two')
+derivative (StarRE r) c = -- (10)
+  let r' = derivative r c
+  in case r' of
+     EmptyRE -> (StarRE r)
+     VoidRE -> VoidRE
+     _ -> (SeqRE r' (StarRE r))
+derivative VoidRE c = VoidRE  -- (11)
+derivative EmptyRE c = VoidRE
+-- END:deriv
